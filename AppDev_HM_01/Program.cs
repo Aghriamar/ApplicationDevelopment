2ee98c469c6a8ede8d82907eea50afa40046f570
Aghriamar <A1exXx88@inbox.ru>
2023-11-18 23:41:35 +0300

    Added Homework for Seminars 6

--- AppDev_HM_01/Program.cs
+++ AppDev_HM_01/Program.cs
@@ -10,6 +10,120 @@ namespace AppDev_HM_01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Простой калькулятор");
+            Console.WriteLine("Для завершения программы введите 'отмена' или пустую строку.");
+
+            while (true)
+            {
+                Console.Write("Введите первое число: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                }
+
+                if (input.ToLower() == "отмена")
+                {
+                    Console.WriteLine("Программа отменена.");
+                    break;
+                }
+
+                if (!DoubleTryParse(input, out double num1))
+                {
+                    continue;
+                }
+
+                Console.Write("Введите оператор (+, -, *, /): ");
+                string operation = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(operation))
+                {
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                }
+
+                if (operation.ToLower() == "отмена")
+                {
+                    Console.WriteLine("Программа отменена.");
+                    break;
+                }
+
+                Console.Write("Введите второе число: ");
+                input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                }
+
+                if (input.ToLower() == "отмена")
+                {
+                    Console.WriteLine("Программа отменена.");
+                    break;
+                }
+
+                if (!DoubleTryParse(input, out double num2))
+                {
+                    continue;
+                }
+
+                double result = 0;
+
+                try
+                {
+                    switch (operation)
+                    {
+                        case "+":
+                            result = num1 + num2;
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            if (result < 0)
+                            {
+                                throw new Exception("Результат не может быть отрицательным.");
+                            }
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            break;
+                        case "/":
+                            if (num2 == 0)
+                            {
+                                throw new Exception("Деление на ноль невозможно.");
+                            }
+                            result = num1 / num2;
+                            if (result < 0)
+                            {
+                                throw new Exception("Результат не может быть отрицательным.");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Неверный оператор.");
+                            continue;
+                    }
+
+                    Console.WriteLine($"Результат: {num1} {operation} {num2} = {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+
+        private static bool DoubleTryParse(string input, out double result)
+        {
+            bool success = double.TryParse(input, out result);
+
+            if (!success)
+            {
+                Console.WriteLine("Неверный формат числа.");
+            }
+
+            return success;
         }
     }
 }
