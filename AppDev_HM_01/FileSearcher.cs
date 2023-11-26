3d8b91b67d0812f4986b0a75983a375ecb95c76b
Aghriamar <A1exXx88@inbox.ru>
2023-11-26 12:27:10 +0300

    Added Homework for Seminars 8

---
+++ AppDev_HM_01/FileSearcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDev_HM_01
+{
+    public class FileSearcher
+    {
+        public void SearchFiles(string directory, string extension, string searchText)
+        {
+            try
+            {
+                var files = Directory.GetFiles(directory, $"*.{extension}", SearchOption.AllDirectories);
+                Console.WriteLine($"Поиск ...");
+                bool found = false;
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        string content = File.ReadAllText(file);
+                        if (content.Contains(searchText))
+                        {
+                            Console.WriteLine($"Найден файл: {file}");
+                            found = true;
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Отказано в доступе: {file}");
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine($"Файл не найден: {file}");
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine($"Ошибка ввода-вывода: {ex.Message}");
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine($"Не найдено файлов с расширением '{extension}' и содержащих текст: '{searchText}'");
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine($"Указанный каталог не найден. {directory}");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine($"Отказано в доступе к указанному каталогу. {directory}");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Ошибка ввода-вывода: {ex.Message}");
+            }
+        }
+    }
+}
--- AppDev_HM_01/Program.cs
+++ AppDev_HM_01/Program.cs
@@ -10,6 +10,21 @@ namespace AppDev_HM_01
     {
         static void Main(string[] args)
         {
+            //args = new string[] { "txt", "куку"};
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Неверное количество аргументов. Укажите расширение файла и текст для поиска.");
+                return;
+            }
+
+            string extension = args[0];
+            string searchText = args[1];
+
+            Console.WriteLine($"Поиск файлов с расширением '{extension}' и содержащих текст: '{searchText}'");
+
+            FileSearcher fileSearcher = new FileSearcher();
+            fileSearcher.SearchFiles(Environment.CurrentDirectory, extension, searchText);
+            Console.ReadLine();
         }
     }
 }
