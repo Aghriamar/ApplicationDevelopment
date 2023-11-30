b5f762711f0c32d82d2118e0d2a36d2bb75b082d
Aghriamar <A1exXx88@inbox.ru>
2023-12-01 00:08:44 +0300

    Added Homework for Seminars 9

--- AppDev_HM_01/Program.cs
+++ AppDev_HM_01/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Text.JSon;
 
 namespace AppDev_HM_01
 {
@@ -10,6 +9,50 @@ namespace AppDev_HM_01
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Не указан путь к файлу JSON.");
+                return;
+            }
+
+            string jsonFilePath = args[0];
+
+            if (!File.Exists(jsonFilePath))
+            {
+                Console.WriteLine("Указанный файл JSON не найден.");
+                return;
+            }
+
+            try
+            {
+                string jsonContent = File.ReadAllText(jsonFilePath);
+
+                using (JsonDocument doc = JsonDocument.Parse(jsonContent))
+                {
+                    // Создаем корневой элемент XML
+                    XElement xmlRoot = new XElement("Root");
+
+                    foreach (JsonProperty prop in doc.RootElement.EnumerateObject())
+                    {
+                        // Добавляем элементы XML, соответствующие полям JSON
+                        XElement xmlElement = new XElement(prop.Name, prop.Value.ToString());
+                        xmlRoot.Add(xmlElement);
+                    }
+
+                    // Сохраняем XML в файл
+                    string xmlFilePath = Path.ChangeExtension(jsonFilePath, ".xml");
+                    xmlRoot.Save(xmlFilePath);
+                    Console.WriteLine($"Конвертация успешно завершена. Файл сохранен по пути: {xmlFilePath}");
+                }
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Ошибка при парсинге JSON: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
         }
     }
 }
